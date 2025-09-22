37d74678f3e33f3ed4753cf7c6bde54044ab6c9a
nasti <anastasiasaienko634@gmail.com>
2025-09-22 19:33:34 +0200

    update all

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
 
-const NoteDetails.client = () => {
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { getSingleNote } from "@/lib/api";
+
+const NoteDetailsClient = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => getSingleNote(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
+
   return (
     <div>
-      
+      <h2>{note.title}</h2>
+      <p>{note.content}</p>
+      <p>{formattedDate}</p>
     </div>
-  )
-}
+  );
+};
 
-export default NoteDetails.client
+export default NoteDetailsClient;
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,35 +1,28 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 import { getSingleNote } from "@/lib/api";
-import { FaEdit, FaStickyNote } from "react-icons/fa";
-import css from "./NoteDetails.module.css";
+import NoteDetailsClient from "./NoteDetails.client";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 const NoteDetails = async ({ params }: Props) => {
-  const { id } = params;
-  const note = await getSingleNote(id);
+  const { id } = await params;
+  const queryClient = new QueryClient();
 
-  const formattedDate = note.updatedAt
-    ? `Updated at: ${new Date(note.updatedAt).toLocaleString()}`
-    : `Created at: ${new Date(note.createdAt).toLocaleString()}`;
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => getSingleNote(id),
+  });
 
   return (
-    <section className={css.details}>
-      <div className={css.header}>
-        <FaStickyNote className={css.icon} />
-        <h2 className={css.noteTitle}>{note.title}</h2>
-      </div>
-
-      <p className={css.content}>{note.content}</p>
-
-      <div className={css.footer}>
-        <p className={css.date}>{formattedDate}</p>
-        <button className={css.button}>
-          <FaEdit className={css.btnIcon} /> Edit
-        </button>
-      </div>
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
   );
 };
 
